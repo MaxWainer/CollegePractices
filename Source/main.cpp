e3a40a1d7209b94f52033f61aaa4b21880408b12
max_wainer <koreisha2000@mail.ru>
2021-11-25 20:34:16 +0300

    F

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,16 +12,17 @@
 #define RED_DIR_PREFIX std::string("ReadDir: ")
 #define CLOSE_DIR_PREFIX std::string("CloseDir: ")
 #define RENAME_PREFIX std::string("Rename: ")
+#define FILE_STAT_PREFIX std::string("FileStat: ")
+#define REGULAR_TYPE 4
 
 using namespace std;
 
-template<typename T>
-string as_string(T t);
+string as_string(long long t);
 
 string last_error();
 
 int main() {
-    string dir_name(".");
+    string dir_name("../tests"); // default dir
 
     DIR *dir = opendir(dir_name.c_str()); // open dir
     if (dir == nullptr)
@@ -30,33 +31,48 @@ int main() {
 
     errno = 0; // clear error
     struct dirent *dir_entry = readdir(dir); // create dirent
-    do {
+    // check first
+    // errno must be null sometimes
+    while (dir_entry) {
         dir_entry = readdir(dir); // read dir/dir_entry
-        string name(dir_entry->d_name); // define name as string (full name)
+        string name(dir_entry->d_name); // define name as string (not full name)
+        string full_name(dir_name + '/' + name);
 
-        if (dir_entry == nullptr) continue; // check is dir/dir_entry null
+//        // check is dir_entry null, if yes, throw an exception
+//        if (dir_entry == nullptr)
+//            throw std::runtime_error(
+//                    RED_DIR_PREFIX + last_error());
 
-        //if (errno != 0)
-        //    throw std::runtime_error(
-        //            RED_DIR_PREFIX + last_error()); // If any error acquired while reading, check and throw error
+        if (errno != 0)
+            throw std::runtime_error(
+                    RED_DIR_PREFIX + last_error()); // If any error acquired while reading, check and throw error
 
         if (name != "." && name != ".." &&
-            (dir_entry->d_type != 4)) { // Else we check is name not equals "." and ".." and dir_entry type is regular
-            if (!name.find("_")) continue; // if name doesn't contain required symbol, skip element
+            (dir_entry->d_type !=
+             REGULAR_TYPE)) { // Else we check is name not equals "." and ".." and dir_entry type is regular
+            if (!name.find('_')) continue; // if name doesn't contain required symbol, skip element
 
-            struct stat file_stat; // create dir_entry stat structure
-            stat(dir_entry->d_name, &file_stat); // fill stat
+            struct stat file_stat; // create dir_entry stat structure, define it as null by default
 
-            string new_name(name + as_string(file_stat.st_size)); // create new name
+            // check stat, if not success, throw an exception
+            if (!stat(dir_entry->d_name, &file_stat))
+                throw std::runtime_error(
+                        FILE_STAT_PREFIX + last_error()); // Check is stat null, if null, throw an exception
 
-            cout << "Renamed: " << name << ", New name: " << new_name << endl;
+            string new_name(full_name + as_string(file_stat.st_size)); // create new name
 
-            //if (!
-            //rename(dir_entry->d_name, new_name.c_str());
-            //) // rename
-            //throw std::runtime_error(RENAME_PREFIX + last_error()); // If any error, throw exception
+            // print info
+            cout << "[LOGGER] File '" << name << "' ('" << full_name << "') size: " << file_stat.st_size << " bytes"
+                 << endl;
+            cout << "[LOGGER] New name will be: " << new_name << endl;
+
+            if (rename(full_name.c_str(), new_name.c_str())) // rename
+                throw std::runtime_error(RENAME_PREFIX + last_error()); // If any error, throw exception
+            else
+                cout << "[LOGGER] Renamed: " << full_name << ", New name: " << new_name
+                     << endl; // print success message
         }
-    } while (dir_entry);
+    }
     if (closedir(dir) != 0)
         throw std::runtime_error(CLOSE_DIR_PREFIX + last_error()); // Check is there any error while closing
     // dir, throw error if yes
@@ -64,13 +80,15 @@ int main() {
     return 0;
 }
 
-string last_error()
-{
-    return std::strerror(errno);
+string last_error() {
+    int buf = errno; // take errno into buffer
+
+    errno = 0; // clear error
+
+    return std::strerror(buf); // return error
 }
 
-template<typename T>
-string as_string(T t) // convert any type to string using stringstream
+string as_string(long long t) // convert any type to string using stringstream
 {
     stringstream s; // create stream
     s << t; // fill with t
